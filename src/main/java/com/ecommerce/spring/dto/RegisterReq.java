82cdfe849e0cebba6b44b7384073515c9dbb2b0b
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2026-03-10 23:28:31 +0530

    fix: fix password length validation in RegisterReq

--- src/main/java/com/ecommerce/spring/dto/RegisterReq.java
+++ src/main/java/com/ecommerce/spring/dto/RegisterReq.java
@@ -1,11 +1,11 @@
 package com.ecommerce.spring.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record RegisterReq(@NotBlank(message = "Username is required") String username,
         @NotBlank(message = "Email is required") @Email(message = "Please provide a valid email address") String email,
-        @NotBlank(message = "Password is required") @Min(value = 6, message = "Password must be at least 6 characters long") String password) {
+        @NotBlank(message = "Password is required") @Size(min = 8, message = "Password must be at least 8 characters long") String password) {
 
 }
